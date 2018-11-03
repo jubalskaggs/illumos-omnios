469ef22073018ab7b0b9b6d8be4f0f724c4f982b
Jubal Skaggs <jubal@vcontext.net>
2018-11-03 04:12:21 -0800

    switch to netbsd vioscsireg.h

--- usr/src/uts/common/io/vioscsi/virtio_scsi.h
+++ usr/src/uts/common/io/vioscsi/virtio_scsi.h
@@ -1,152 +1,85 @@
-/*-
- * This header is BSD licensed so anyone can use the definitions to implement
- * compatible drivers/servers.
+/*	$OpenBSD: vioscsireg.h,v 1.1 2013/12/20 21:50:49 matthew Exp $	*/
+/*
+ * Copyright (c) 2013 Google Inc.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * $FreeBSD: head/sys/dev/virtio/scsi/virtio_scsi.h 241470 2012-10-11 23:41:18Z grehan $
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#ifndef __VIRTIO_SCSI_H__
+#define __VIRTIO_SCSI_H__
 
-#ifndef _VIRTIO_SCSI_H
-#define _VIRTIO_SCSI_H
+#include <sys/types.h>
+
+
+/* Configuration registers */
+#define VIRTIO_SCSI_CONFIG_NUM_QUEUES       0 /* 32bit */
+#define VIRTIO_SCSI_CONFIG_SEG_MAX          4 /* 32bit */
+#define VIRTIO_SCSI_CONFIG_MAX_SECTORS		8 /* 32bit */
+#define VIRTIO_SCSI_CONFIG_CMD_PER_LUN		12 /* 32bit */
+#define VIRTIO_SCSI_CONFIG_EVENT_INFO_SIZE	16 /* 32bit */
+#define VIRTIO_SCSI_CONFIG_SENSE_SIZE		20 /* 32bit */
+#define VIRTIO_SCSI_CONFIG_CDB_SIZE         24 /* 32bit */
+#define VIRTIO_SCSI_CONFIG_MAX_CHANNEL		28 /* 16bit */
+#define VIRTIO_SCSI_CONFIG_MAX_TARGET		30 /* 16bit */
+#define VIRTIO_SCSI_CONFIG_MAX_LUN          32 /* 32bit */
 
 /* Feature bits */
-#define VIRTIO_SCSI_F_INOUT	0x0001	/* Single request can contain both
-                     * read and write buffers */
-#define VIRTIO_SCSI_F_HOTPLUG	0x0002	/* Host should enable hot plug/unplug
-                     * of new LUNs and targets.
-                     */
+#define VIRTIO_SCSI_F_INOUT                 (0x1 << 0)
+#define VIRTIO_SCSI_F_HOTPLUG               (0x1 << 1)
 
-#define VIRTIO_SCSI_CDB_SIZE	32
-#define VIRTIO_SCSI_SENSE_SIZE	96
+/* Response status values */
+#define VIRTIO_SCSI_S_OK                    0
+#define VIRTIO_SCSI_S_OVERRUN               1
+#define VIRTIO_SCSI_S_ABORTED               2
+#define VIRTIO_SCSI_S_BAD_TARGET            3
+#define VIRTIO_SCSI_S_RESET                 4
+#define VIRTIO_SCSI_S_BUSY                  5
+#define VIRTIO_SCSI_S_TRANSPORT_FAILURE		6
+#define VIRTIO_SCSI_S_TARGET_FAILURE		7
+#define VIRTIO_SCSI_S_NEXUS_FAILURE         8
+#define VIRTIO_SCSI_S_FAILURE               9
 
-/* SCSI command request, followed by data-out */
-struct virtio_scsi_cmd_req {
-    uint8_t lun[8];		/* Logical Unit Number */
-    uint64_t tag;		/* Command identifier */
-    uint8_t task_attr;	/* Task attribute */
-    uint8_t prio;
-    uint8_t crn;
-    uint8_t cdb[VIRTIO_SCSI_CDB_SIZE];
+/* Task attributes */
+#define VIRTIO_SCSI_S_SIMPLE                0
+#define VIRTIO_SCSI_S_ORDERED               1
+#define VIRTIO_SCSI_S_HEAD                  2
+#define VIRTIO_SCSI_S_ACA                   3
+
+#ifndef __packed
+#define __packed __attribute__((packed))
+#endif
+
+/* Request header structure */
+struct virtio_scsi_req_hdr {
+    uint8_t		lun[8];
+    uint64_t	id;
+    uint8_t		task_attr;
+    uint8_t		prio;
+    uint8_t		crn;
+    uint8_t		cdb[32];
 } __packed;
+/* Followed by data-out. */
 
-/* Response, followed by sense data and data-in */
-struct virtio_scsi_cmd_resp {
-    uint32_t sense_len;		/* Sense data length */
-    uint32_t resid;			/* Residual bytes in data buffer */
-    uint16_t status_qualifier;	/* Status qualifier */
-    uint8_t status;			/* Command completion status */
-    uint8_t response;		/* Response values */
-    uint8_t sense[VIRTIO_SCSI_SENSE_SIZE];
+/* Response header structure */
+struct virtio_scsi_res_hdr {
+    uint32_t	sense_len;
+    uint32_t	residual;
+    uint16_t	status_qualifier;
+    uint8_t		status;
+    uint8_t		response;
+    uint8_t		sense[96];
 } __packed;
+/* Followed by data-in. */
 
-/* Task Management Request */
-struct virtio_scsi_ctrl_tmf_req {
-    uint32_t type;
-    uint32_t subtype;
-    uint8_t lun[8];
-    uint64_t tag;
-} __packed;
 
-struct virtio_scsi_ctrl_tmf_resp {
-    uint8_t response;
-} __packed;
 
-/* Asynchronous notification query/subscription */
-struct virtio_scsi_ctrl_an_req {
-    uint32_t type;
-    uint8_t lun[8];
-    uint32_t event_requested;
-} __packed;
-
-struct virtio_scsi_ctrl_an_resp {
-    uint32_t event_actual;
-    uint8_t response;
-} __packed;
-
-struct virtio_scsi_event {
-    uint32_t event;
-    uint8_t lun[8];
-    uint32_t reason;
-} __packed;
-
-struct virtio_scsi_config {
-    uint32_t num_queues;
-    uint32_t seg_max;
-    uint32_t max_sectors;
-    uint32_t cmd_per_lun;
-    uint32_t event_info_size;
-    uint32_t sense_size;
-    uint32_t cdb_size;
-    uint16_t max_channel;
-    uint16_t max_target;
-    uint32_t max_lun;
-} __packed;
-
-/* Response codes */
-#define VIRTIO_SCSI_S_OK                       0
-#define VIRTIO_SCSI_S_FUNCTION_COMPLETE        0
-#define VIRTIO_SCSI_S_OVERRUN                  1
-#define VIRTIO_SCSI_S_ABORTED                  2
-#define VIRTIO_SCSI_S_BAD_TARGET               3
-#define VIRTIO_SCSI_S_RESET                    4
-#define VIRTIO_SCSI_S_BUSY                     5
-#define VIRTIO_SCSI_S_TRANSPORT_FAILURE        6
-#define VIRTIO_SCSI_S_TARGET_FAILURE           7
-#define VIRTIO_SCSI_S_NEXUS_FAILURE            8
-#define VIRTIO_SCSI_S_FAILURE                  9
-#define VIRTIO_SCSI_S_FUNCTION_SUCCEEDED       10
-#define VIRTIO_SCSI_S_FUNCTION_REJECTED        11
-#define VIRTIO_SCSI_S_INCORRECT_LUN            12
-
-/* Controlq type codes.  */
-#define VIRTIO_SCSI_T_TMF                      0
-#define VIRTIO_SCSI_T_AN_QUERY                 1
-#define VIRTIO_SCSI_T_AN_SUBSCRIBE             2
-
-/* Valid TMF subtypes.  */
-#define VIRTIO_SCSI_T_TMF_ABORT_TASK           0
-#define VIRTIO_SCSI_T_TMF_ABORT_TASK_SET       1
-#define VIRTIO_SCSI_T_TMF_CLEAR_ACA            2
-#define VIRTIO_SCSI_T_TMF_CLEAR_TASK_SET       3
-#define VIRTIO_SCSI_T_TMF_I_T_NEXUS_RESET      4
-#define VIRTIO_SCSI_T_TMF_LOGICAL_UNIT_RESET   5
-#define VIRTIO_SCSI_T_TMF_QUERY_TASK           6
-#define VIRTIO_SCSI_T_TMF_QUERY_TASK_SET       7
-
-/* Events.  */
-#define VIRTIO_SCSI_T_EVENTS_MISSED            0x80000000
-#define VIRTIO_SCSI_T_NO_EVENT                 0
-#define VIRTIO_SCSI_T_TRANSPORT_RESET          1
-#define VIRTIO_SCSI_T_ASYNC_NOTIFY             2
-
-/* Reasons of transport reset event */
-#define VIRTIO_SCSI_EVT_RESET_HARD             0
-#define VIRTIO_SCSI_EVT_RESET_RESCAN           1
-#define VIRTIO_SCSI_EVT_RESET_REMOVED          2
-
-#define VIRTIO_SCSI_S_SIMPLE                   0
-#define VIRTIO_SCSI_S_ORDERED                  1
-#define VIRTIO_SCSI_S_HEAD                     2
-#define VIRTIO_SCSI_S_ACA                      3
-
-#endif /* _VIRTIO_SCSI_H */
+#endif /* __VIRTIO_SCSI_H__ */
